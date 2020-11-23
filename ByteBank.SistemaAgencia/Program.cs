dd74731238e2d7e25d66c2cf5e917b19813a5f15
Wesley <wesley.contato@hotmail.com>
2020-11-23 04:29:11 -0300

    Valores padrões em arrays

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -14,19 +14,19 @@ namespace ByteBank.SistemaAgencia
         {
             // array de inteiros com 5 posições
 
-            int[] idades = new int[5];
+            int[] idades = null;
+            idades = new int[5];
 
             idades[0] = 15;
             idades[1] = 28;
             idades[2] = 35;
             idades[3] = 50;
-            idades[4] = 28;
+            idades[4] = 22;
 
+            int indice = 4;
+            int idadeNoIndice4 = idades[indice];
 
-            Console.WriteLine(idades[4]);
-
-
-
+            Console.WriteLine(idadeNoIndice4);
 
 
             Console.ReadLine();
