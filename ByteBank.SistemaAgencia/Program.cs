e26af0ebdebd5b983f8e4671d289e82d480252b4
Wesley <wesley.contato@hotmail.com>
2020-11-23 04:15:22 -0300

    Conhecendo array

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -12,6 +12,23 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+            // array de inteiros com 5 posições
+
+            int[] idades = new int[5];
+
+            idades[0] = 15;
+            idades[1] = 28;
+            idades[2] = 35;
+            idades[3] = 50;
+            idades[4] = 28;
+
+
+            Console.WriteLine(idades[4]);
+
+
+
+
+
             Console.ReadLine();
         }
     }
